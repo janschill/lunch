ee8d59e096f7b7ce963e66911b9aae95ed94063b
Jan Schill <schill@hey.com>
2021-11-29 13:07:38 +0100

    Add more lunch places

--- app.rb
+++ app.rb
@@ -3,13 +3,15 @@ require "erb"
 
 get "/" do
   @lunch = %w(
-    Mums
-    Meno-Ramen
+    Mums-deli
+    Men-o-Ramen
     Wokbox
     Vandkunsten
     Garbanzo
     Lagkagehuset
     American-Pie-Co
+    District-Tonkin
+    Domhusets-Smørrebrød
   ).sample
   erb :index
 end
